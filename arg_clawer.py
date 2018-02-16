5a409c6fda69fd29c994f691cafecf9e0a6778bb
Man Chong Leong <ml69@rice.edu>
2018-02-16 11:12:30 -0600

    make a try catch conditions

--- arg_clawer.py
+++ arg_clawer.py
@@ -1,10 +1,14 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
 import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="enter the website url")
     parser.add_argument("-url", help="enter the link")
     args = parser.parse_args()
-    htmlcontext = urlopen(args.url)
-    print(htmlcontext.read())
-
+    try:
+        htmlcontext = urlopen(args.url)
+    except HTTPError as e:
+        print("Error appears as followings: \n", e)
+    else:
+        print(htmlcontext.read())
