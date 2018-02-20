7b760058beaec60f036b7b5ab6c031c01d85ddc9
Man Chong Leong <ml69@rice.edu>
2018-02-20 14:28:43 -0600

    picture crawler #3

---
+++ pic_clawer.py
@@ -0,0 +1,22 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+import argparse
+from bs4 import BeautifulSoup
+import re
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="enter the website url")
+    parser.add_argument("-url", help="enter the link")
+    args = parser.parse_args()
+    try:
+        htmlcontext = urlopen(args.url)
+    except HTTPError as e:
+        print("Error appears as followings: \n", e)
+        htmlcontext = None
+    else:
+        bsOb4 = BeautifulSoup(htmlcontext,"html.parser")
+        context = bsOb4.findAll("img",{"src":re.compile(".*\.(jpg|bmp|gif|png)")})
+        for img in context:
+            print(img["src"])
+    if htmlcontext == None:
+        print("Somethings went wrong, please check the entered url.")
