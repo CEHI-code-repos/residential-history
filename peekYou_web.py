e9b7b2b0384ee2bdcb2dcdc8f43d4ff8f258190a
Man Chong Leong <ml69@rice.edu>
2018-05-02 14:26:21 -0500

    code for peekYou

---
+++ peekYou_web.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import pandas as pd
+fileName = '/Users/manchongleong/Desktop/nameCEHI.csv'
+rawData = pd.read_csv(fileName, low_memory=False)
+inputData = rawData['Name'].tolist()
+for i in range(len(inputData)):
+    dr = webdriver.Chrome('/Users/manchongleong/Desktop/chromedriver')
+    dr.get("https://www.peekyou.com/")
+    elemFirstName = dr.find_element_by_id("fname")
+    elemLastName = dr.find_element_by_id("lname")
+    print(inputData[i])
+    elemFirstName.send_keys(inputData[i].split(" ")[0])
+    elemLastName.send_keys(inputData[i].split(" ")[1])
+    elemLastName.send_keys(Keys.RETURN)
+    time.sleep(2)
+    dr.find_element_by_xpath('//*[@id="beenverified_results_list"]/ul/li/div[2]/p[1]/a[1]').click()
+    with open('./' + inputData[i] + '.html', "w") as f:
+        f.write(dr.page_source)
+    dr.close()
