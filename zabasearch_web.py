08440685d18912166ac4906b6e081f66649c5197
Man Chong Leong <ml69@rice.edu>
2018-05-02 14:25:33 -0500

    code for zabasearch

---
+++ zabasearch_web.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+import pandas as pd
+from selenium.webdriver.common.keys import Keys
+import time
+fileName = '/Users/manchongleong/Desktop/nameCEHI.csv'
+rawData = pd.read_csv(fileName, low_memory=False)
+inputData = rawData['Name'].tolist()
+for i in range(len(inputData)):
+    dr = webdriver.Chrome('/Users/manchongleong/Desktop/chromedriver')
+    dr.get("http://www.zabasearch.com/")
+    elem = dr.find_element_by_xpath('''//*[@id="search-box"]''')
+    print(inputData[i])
+    elem.send_keys(inputData[i])
+    elem.send_keys(Keys.RETURN)
+    time.sleep(2)
+    dr.find_element_by_xpath('//*[@id="content-container"]/div[2]/div[5]/div[1]/strong/a').click()
+    with open('./' + inputData[i] + '.html', "w") as f:
+        f.write(dr.page_source)
+    dr.close()
